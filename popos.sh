7bb52371dbfaeee0cd1a6b1823ee60874887b202
iTzBoboCz <bobo12002@icloud.com>
2020-09-19 18:32:20 +0200

    Update popos.sh

--- popos.sh
+++ popos.sh
@@ -105,9 +105,8 @@ gsettings set org.gnome.desktop.calendar show-weekdate true
 
 #Install Dash-to-Dock
 cd ~/.local/share/gnome-shell/extensions \
-&& sudo curl -o dash-to-dock@micxgx.gmail.com.zip https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v68.shell-extension.zip \
-&& sudo gnome-extensions install dash-to-dock@micxgx.gmail.com.zip \
-&& sudo gnome-extensions enable dash-to-dock@micxgx.gmail.com \
+&& sudo curl -o dash-to-dock@micxgx.gmail.com.shell-extension.zip https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v68.shell-extension.zip \
+&& gnome-extensions install dash-to-dock@micxgx.gmail.com.shell-extension.zip \
 && sudo rm dash-to-dock@micxgx.gmail.com.shell-extension.zip \
 && cd ~ \
 && sudo gnome-shell --replace
